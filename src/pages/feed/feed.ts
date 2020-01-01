e3cb4b64c9fae5cecb563fa8eed451020f4ff106
Sidhant Gupta <sdhnt@connect.hku.hk>
2020-01-02 03:03:57 +0800

    slot conflict, img UI

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -10,19 +10,19 @@ const axios = require('axios');
 })
 export class FeedPage {
   // cors = cors({origin: true});
-  text: string="";
-  posts: any[]=[];
-  pageSize: number= 10;
+  text: string = "";
+  posts: any[] = [];
+  pageSize: number = 10;
   cursor: any;//documentSnapshot- holds value of pageSizeth post
   infiniteEvent: any;
-  image: string; 
-  retrieved_images: string[]=[];
+  image: string;
+  retrieved_images: string[] = [];
   retrieved_image: string;
   all_locations: any;
   endDate: string;
   startTime: any;
   endTime: any = new Date();
-  screenlog:any;
+  screenlog: any;
   testPic: any;
   timeslot: string;
   schedule: any = [];
@@ -32,225 +32,245 @@ export class FeedPage {
   ISOTime: string;
   TZ: any;
   ShutterTime: string;
-  location:number;
+  location: number;
   today: string = new Date().toISOString(); // minimum date = current date
   startDate: string;
   min_end_date: string = this.startDate;
   maxDate: string = new Date(new Date().getFullYear(), new Date().getMonth() + 3, new Date().getDate()).toISOString(); // max date = 3 months from today
   feedlog: string;
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams,public loadingCtrl: LoadingController, 
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController,
     public toastCtrl: ToastController,
     public alertCtrl: AlertController,
-    ) {
-      
+  ) {
+
   }
 
-  timezone=8;
+  timezone = 8;
 
- 
-addSlot()
-{
-  if(this.startDate==null || this.endDate == null){
-    this.alertCtrl.create({
-      title: "Error",
-      message: "Need to Specify Start and End Time",
-      buttons: [{
-        text: 'OK',
-        role: 'cancel'
-      },
-      ]
-    }).present()
-    return;
-  }
-  if(this.startDate>=this.endDate)
-  {
-    this.alertCtrl.create({
-      title: "Error",
-      message: "End time must be greater than start time"
-    }).present()
-    return;
-  }
 
-  var vid;
-  if(this.photovid=="true"){
-    vid=true;
-  }else{ //its a photo
-    vid=false;
-    if(this.photovid==null){
+  addSlot() {
+    if (this.startDate == null || this.endDate == null) {
       this.alertCtrl.create({
         title: "Error",
-        message: "Either Photo or video mode must be selected"
+        message: "Need to Specify Start and End Time",
+        buttons: [{
+          text: 'OK',
+          role: 'cancel'
+        },
+        ]
       }).present()
       return;
     }
-  }
-  console.log(vid)
+    if (this.startDate >= this.endDate) {
+      this.alertCtrl.create({
+        title: "Error",
+        message: "End time must be greater than start time"
+      }).present()
+      return;
+    }
 
-  if(this.photovid=="false"){
-    if(this.ISOTime==undefined ||this.ShutterTime==undefined || this.intervalTime==undefined){
-   
+    var vid;
+    if (this.photovid == "true") {
+      vid = true;
+    } else { //its a photo
+      vid = false;
+      if (this.photovid == null) {
+        this.alertCtrl.create({
+          title: "Error",
+          message: "Either Photo or video mode must be selected"
+        }).present()
+        return;
+      }
+    }
+    console.log(vid)
+
+    if (this.photovid == "false") {
+      if (this.ISOTime == undefined || this.ShutterTime == undefined || this.intervalTime == undefined) {
+
         this.alertCtrl.create({
           title: "Error",
           message: "Please fill in all the fields (ISO, Shutter, Interval)"
         }).present()
         return;
+      }
     }
+    var tempslot = {
+
+      start: this.startDate.substring(0, this.startDate.length - 10) + "-" + this.startDate.substring(11, this.startDate.length - 1),//remove Z  
+      stop: this.endDate.substring(0, this.endDate.length - 10) + "-" + this.endDate.substring(11, this.endDate.length - 1),//remove Z  
+      iso: parseInt(this.ISOTime),
+      frequency: parseInt(this.intervalTime),
+      shutter_speed: parseInt(this.ShutterTime),
+      video: vid,
+
+    }
+
+    var flag = 0;
+
+    this.schedule.forEach(element => {
+      if ((element.stop > tempslot.start && element.start > tempslot.start) || (element.start < tempslot.start && element.stop < tempslot.start)) {
+        console.log('no conflict')
+
+      } else {
+
+        this.alertCtrl.create({
+          title: "Error",
+          message: "Time-Slot Conflict with: (" + element.start + "-" + element.stop + ")",
+          buttons: [{
+            text: 'OK',
+            role: 'cancel'
+          },
+          ]
+        }).present()
+        flag = 1;
+      }
+
+    });
+    if (flag == 0) {
+      this.timeslot = JSON.stringify(tempslot)
+      this.schedule.push(tempslot);
+      this.printedschedule = JSON.stringify(this.schedule);
+    }
+
   }
-  var tempslot = {
 
-    start: this.startDate.substring(0,this.startDate.length -10)+"-"+this.startDate.substring(11,this.startDate.length -1),//remove Z  
-    stop: this.endDate.substring(0,this.endDate.length -10)+"-"+this.endDate.substring(11,this.endDate.length -1),//remove Z  
-    iso: parseInt(this.ISOTime),
-    frequency: parseInt(this.intervalTime),
-    shutter_speed: parseInt(this.ShutterTime),
-    video: vid,
-    
-  }
+  headers = { 'Access-Control-Allow-Origin': 'localhost, *', 'Access-Control-Allow-Methods': 'GET, OPTIONS, POST', "Access-Control-Allow-Headers": "Access-Control-*,", }
 
-  this.timeslot=JSON.stringify(tempslot)
-  this.schedule.push(tempslot);
-  this.printedschedule=JSON.stringify(this.schedule);
+  async sendConfig() {
 
-}
-
-headers = {'Access-Control-Allow-Origin': 'localhost, *', 'Access-Control-Allow-Methods': 'GET, OPTIONS, POST', "Access-Control-Allow-Headers": "Access-Control-*,",}
- 
-async sendConfig()
-{
-
-  var senddtt= new Date()
+    var senddtt = new Date()
 
 
-  var senddt=senddtt.toISOString();
+    var senddt = senddtt.toISOString();
 
-  var a=(senddtt.getHours());
-  var b=senddtt.getMinutes();
-  var c=senddtt.getSeconds()
-  var a1; var b1; var c1;
-  if(a<=9){
-    a1="0"+a.toString();
-  }
-  else
-  {a1=a.toString()}
+    var a = (senddtt.getHours());
+    var b = senddtt.getMinutes();
+    var c = senddtt.getSeconds()
+    var a1; var b1; var c1;
+    if (a <= 9) {
+      a1 = "0" + a.toString();
+    }
+    else { a1 = a.toString() }
 
-  if(b<=9){
-    b1="0"+b.toString();
-  }
-  else
-  {b1=b.toString()}
+    if (b <= 9) {
+      b1 = "0" + b.toString();
+    }
+    else { b1 = b.toString() }
 
-  if(c<=9){
-    c1="0"+c.toString();
-  }
-  else
-  {c1=c.toString()}
-
-  
+    if (c <= 9) {
+      c1 = "0" + c.toString();
+    }
+    else { c1 = c.toString() }
 
 
-  this.schedule[0].date=senddt.substring(0,10)+" "+a1+":"+b1+":"+c1,
 
 
-  console.log("DTString: "+this.schedule[0].date+" "+senddtt)
-  
-  axios({
-    method: 'post',
-    url: 'http://192.168.5.1:8000/setSchedule',
-    //withCredentials: true, // True otherwise I receive another error
-    headers: this.headers,
-    data: this.schedule,
-  }).then(response => {
-    console.log('Logout ', response.data.success);
-    this.feedlog="Success";
-  }).catch(()=>{
-    console.log('No Response ');
-  })
-  
-  // axios.post('http://169.254.52.217:8000/setSchedule/', this.schedule, headers)
-  // .then(function (response) {
-  //   console.log(response);
-  // })
-  // .catch(function (error) {
-  //   console.log(error);
-  // });
-}
+    this.schedule[0].date = senddt.substring(0, 10) + " " + a1 + ":" + b1 + ":" + c1,
 
-async viewConfig() {
-  try {
-  
-    //const response = await axios.get('http://169.254.52.217:8000/viewConfig', {});
-    const response = await axios({
-      method: 'get',
-      url: 'http://192.168.5.1:8000/viewConfig',
+
+      console.log("DTString: " + this.schedule[0].date + " " + senddtt)
+
+    axios({
+      method: 'post',
+      url: 'http://192.168.5.1:8000/setSchedule',
+      //withCredentials: true, // True otherwise I receive another error
       headers: this.headers,
+      data: this.schedule,
+    }).then(response => {
+      console.log('Logout ', response.data.success);
+      this.feedlog = "Success";
+    }).catch(() => {
+      console.log('No Response ');
     })
-      
-    this.schedule=response.data;
-    if(this.schedule.error!=null)
-    {
-      this.feedlog="No Config Exists. Please set a configuration first.";
-    }
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-}  
 
-async testPhoto(){
-  try {
-  
-    //const response = await axios.get('http://169.254.52.217:8000/testPhoto', Headers, {});
-    const response = await axios({
-      method: 'get',
-      url: 'http://192.168.5.1:8000/testPhoto',
-      headers: this.headers,
-    })
-    var base64Data=response.data;
-    this.testPic= "data:image/jpeg;base64,"+base64Data;
-    //console.log(base64Data)
-    
-
-    // const bytes: string = atob(base64Data);
-    //   const byteNumbers = new Array(bytes.length);
-    //   for (let i = 0; i < bytes.length; i++) {
-    //     byteNumbers[i] = bytes.charCodeAt(i);
-    //   }
-    //   const byteArray = new Uint8Array(byteNumbers);
-
-    //   const blob: Blob = new Blob([byteArray], { type: 'image/png' });
-
-    //   this.testPic= blob;
-    // var b64 = window.btoa(unescape(encodeURIComponent(base64Data)))
-    // console.log(b64);
-    // this.testPic=b64;
-
-  
-
-    console.log(response);
-  } catch (error) {
-    console.error(error); 
+    // axios.post('http://169.254.52.217:8000/setSchedule/', this.schedule, headers)
+    // .then(function (response) {
+    //   console.log(response);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
   }
 
-}
+  async viewConfig() {
+    try {
 
-remitem(tempslot){
-  
-  this.schedule.forEach((element, index) => {
-    if(element=tempslot){
-      this.schedule.splice(index, 1);
+      //const response = await axios.get('http://169.254.52.217:8000/viewConfig', {});
+      const response = await axios({
+        method: 'get',
+        url: 'http://192.168.5.1:8000/viewConfig',
+        headers: this.headers,
+      })
+
+      this.schedule = response.data;
+      if (this.schedule.error != null) {
+        this.feedlog = "No Config Exists. Please set a configuration first.";
+      }
+      console.log(response);
+    } catch (error) {
+      console.error(error);
     }
-  });
-}
-  
+  }
+
+  closetestimg(){
+    this.testPic=null;
+  }
+
+  async testPhoto() {
+    try {
+
+      //const response = await axios.get('http://169.254.52.217:8000/testPhoto', Headers, {});
+      const response = await axios({
+        method: 'get',
+        url: 'http://192.168.5.1:8000/testPhoto',
+        headers: this.headers,
+      })
+      var base64Data = response.data;
+      this.testPic = "data:image/jpeg;base64," + base64Data;
+      //console.log(base64Data)
+
+
+      // const bytes: string = atob(base64Data);
+      //   const byteNumbers = new Array(bytes.length);
+      //   for (let i = 0; i < bytes.length; i++) {
+      //     byteNumbers[i] = bytes.charCodeAt(i);
+      //   }
+      //   const byteArray = new Uint8Array(byteNumbers);
+
+      //   const blob: Blob = new Blob([byteArray], { type: 'image/png' });
+
+      //   this.testPic= blob;
+      // var b64 = window.btoa(unescape(encodeURIComponent(base64Data)))
+      // console.log(b64);
+      // this.testPic=b64;
+
+
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  remitem(tempslot) {
+
+    this.schedule.forEach((element, index) => {
+      if (element = tempslot) {
+        this.schedule.splice(index, 1);
+      }
+    });
+  }
 
 
 
 
-editConfig(){
 
-  
-}
+  editConfig() {
+
+
+  }
 
 
 
